5950c20a53321ad41202de323cc93820fafff294
Souvik De <souvik.de@sato-global.com>
2024-05-23 11:49:21 +0900

    add leetcode 2597 - The Number of Beautiful Subsets

---
+++ Daily-Leetcode/2597.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int v[1001];
+    int solve(int i, vector<int> &nums, int k){
+        if(i == nums.size())
+            return 1;
+        int taken = 0;
+        if(nums[i]-k < 0 || v[nums[i]-k]==0){
+            v[nums[i]]++;
+            taken = solve(i+1,nums, k);
+            v[nums[i]]--;
+        }
+        int notTaken = solve(i+1, nums, k);
+        return taken + notTaken;
+    }
+    int beautifulSubsets(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        memset(v, 0, sizeof(v));
+        return solve(0, nums, k) - 1;        
+    }
+};
