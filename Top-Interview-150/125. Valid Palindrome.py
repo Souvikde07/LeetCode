a58ce64cd1b5a5ac1b50897016d23be7ff4e4465
Souvik De <souvik.de@sato-global.com>
2024-05-23 11:00:35 +0900

    Valid Palindrome

---
+++ Top-Interview-150/125. Valid Palindrome.py
@@ -0,0 +1,29 @@
+# Brute-force Approach
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]   #compare the new string with the reversed string.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+
+        while l<r:
+            while l<r and not self.alphanum(s[l]):
+                l += 1
+            while r>l and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
+    def alphanum(self, c):
+        return (ord('A')<=ord(c)<=ord('Z') or
+                ord('a')<=ord(c)<=ord('z') or
+                ord('0')<=ord(c)<=ord('9'))  #ascii val in python is obtained from ord
+
