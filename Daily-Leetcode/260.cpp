1d4326c3c4bbbd3369e9ea33f8b99ff24dc9ae21
Souvik De <souvik.de@sato-global.com>
2024-05-31 18:23:50 +0900

    add leetcode 260 - Single Number III

---
+++ Daily-Leetcode/260.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long xor_r = 0;
+        for(int &num : nums){
+            xor_r ^= num;
+        }
+        //mask -> right most set bit search
+        int mask = (xor_r) & (-xor_r);
+        int groupa = 0;
+        int groupb = 0;
+
+        for(int &num : nums){
+            if(num & mask){
+                groupa ^= num;
+            }
+            else{
+                groupb ^= num;
+            }
+        }
+        return {groupa, groupb};
+    }
+};
