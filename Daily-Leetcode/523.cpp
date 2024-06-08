5dfc4121e4f3e3a3347cf8d03bfc485039926736
Souvik De <souvik.de@sato-global.com>
2024-06-08 16:49:44 +0900

    add leetcode 523 - Continuous Subarray Sum

---
+++ Daily-Leetcode/523.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+        mp[0] = -1;
+
+        for(int i = 0;i < n; i++){
+            sum += nums[i];
+            int remainder = sum % k;
+            if(mp.find(remainder)!=mp.end()){
+                if(i-mp[remainder]>=2)
+                    return true;
+            }else
+                mp[remainder] = i;
+        }
+        return false;
+    }
+};
