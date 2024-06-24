435d4b198bf15bb81eac0b347b5e08422cc3b8ff
Souvik De <souvik.de@sato-global.com>
2024-06-24 11:52:09 +0900

    add leetcode 995 - Minimum Number of K Consecutive Bit Flips

--- Daily-Leetcode/995.cpp
+++ Daily-Leetcode/995.cpp
@@ -1,3 +1,4 @@
+//Approach - 1 - TC - O(N), SC = O(1)
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
@@ -11,6 +12,30 @@ public:
                 if(i + k > n) return - 1;
                 nums[i] += 2;
                 cur++;
+                res++;
+            }
+        }
+        return res;
+    }
+};
+//Approach - 2 With Space Complexity = O(n)- due to the isFlipped array used.
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int flipped = 0;
+        int res = 0;
+        vector<int> isFlipped(n, 0);
+        for(int i = 0; i < n; i++){
+            if(i >= k){
+                flipped ^= isFlipped[i-k];
+            }
+            if(flipped == nums[i]){
+                if(i + k > n){
+                    return -1;
+                }
+                isFlipped[i] = 1;
+                flipped ^= 1;
                 res++;
             }
         }
