391b1758656e910e2cd9038d42c2b50ca2134aab
Souvik De <souvik.de@sato-global.com>
2024-06-28 15:47:36 +0900

    add leetcode 2285 - Maximum Total Importance of Roads

---
+++ Daily-Leetcode/2285.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n,0); // S.C = O(n)
+        for(auto &vec : roads){
+            int u = vec[0];
+            int v = vec[1];
+            degree[u]++;
+            degree[v]++;
+        }
+        sort(begin(degree), end(degree)); // O(nlog(n))
+        long long value = 1;
+        long long sum = 0;
+        for(int i= 0; i < n; i++){ //O(n)
+            sum += (degree[i] * value);
+            value++;
+        }
+        return sum;
+    }
+};
