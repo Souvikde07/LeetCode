5663826950709e175aae3dd00a329405b9f14dd5
Souvik De <souvik.de@sato-global.com>
2024-07-04 12:43:10 +0900

    add leetcode 2181 - Merge Nodes in Between Zeros

---
+++ Daily-Leetcode/2181.cpp
@@ -0,0 +1,40 @@
+//Approach-1
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* P1 = head->next;
+        ListNode* P2 = P1;
+        while(P2 != NULL){
+            int sum = 0;
+            while(P2 !=NULL && P2 -> val != 0){
+                sum += P2 -> val;
+                P2 = P2->next;
+            }
+            P1->val = sum;
+            P2 = P2->next;
+            P1->next = P2;
+            P1 = P1->next;
+        }
+        return head->next;
+    }
+};
+
+//Approach-2-Using Recursion
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        head = head->next;
+        if(head==NULL){
+            return head;
+        }
+        ListNode* temp = head;
+        int sum = 0;
+        while(temp != NULL && temp->val != 0){
+            sum += temp->val;
+            temp = temp->next;
+        }
+        head->val = sum;
+        head->next = mergeNodes(temp); //Recursion
+        return head;
+    }
+};
