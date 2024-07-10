1ce9158102d76d435fd5785809462450648efb27
Souvik De <souvik.de@sato-global.com>
2024-07-10 14:11:22 +0900

    add leetcode 1598 - Crawler Log Folder

--- Daily-Leetcode/1598.cpp
+++ Daily-Leetcode/1598.cpp
@@ -17,3 +17,19 @@ public:
     }
 };
 //Approach - 2
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for(string &log : logs){
+            if(log == "../"){
+                if(!st.empty()){
+                    st.pop();
+                }
+            } else if(log != "./"){
+                st.push(log);
+            }
+        }
+        return st.size();
+    }
+};
