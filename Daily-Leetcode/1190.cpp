65ade440f3575dee0a6e7b0add59b9d11e08e824
Souvik De <souvik.de@sato-global.com>
2024-07-11 14:59:13 +0900

    add leetcode 1190 - Reverse Substrings Between Each Pair of Parentheses

--- Daily-Leetcode/1190.cpp
+++ Daily-Leetcode/1190.cpp
@@ -20,3 +20,24 @@ public:
         return current;
     }
 };
+//Approach-2-TC-O(n*n) - Simple Method using stack
+// TC = O(n*n)
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> lastSkipLength;
+        string result;
+        for(char &ch: s){
+            if(ch == '('){
+                lastSkipLength.push(result.length());
+            } else if(ch == ')'){
+                int l = lastSkipLength.top();
+                lastSkipLength.pop();
+                reverse(begin(result) + l, end(result));
+            } else{
+                result.push_back(ch);
+            }
+        }
+        return result;
+    }
+};
