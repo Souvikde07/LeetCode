d546857de23d5840c40c97bab42458ef5addc9f0
Souvik De <souvik.de@sato-global.com>
2024-07-15 18:26:26 +0900

    add leetcode 2196 - Create Binary Tree From Descriptions

---
+++ Daily-Leetcode/2196.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ //s.c. = O(n)
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {//O(n)
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> childSet;
+        for(vector<int>&vec : descriptions){
+            int parent = vec[0];
+            int child = vec[1];
+            int isLeft = vec[2];
+            if(mp.find(parent) == mp.end()){
+                mp[parent] = new TreeNode(parent);
+            }
+            if(mp.find(child) == mp.end()){
+                mp[child] = new TreeNode(child);
+            }
+            if (isLeft == 1){
+                mp[parent]->left = mp[child];
+            } else{
+                mp[parent]->right = mp[child];
+            }
+            childSet.insert(child);
+        }
+        for(vector<int>& vec : descriptions){//O(n)
+            int parent = vec[0];
+            if(childSet.find(parent) == childSet.end()){
+                return mp[parent];
+            }
+        }
+        return NULL;
+    }
+};
