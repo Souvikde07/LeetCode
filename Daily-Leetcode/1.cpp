70d0cc0a46a0e589837ae08b9a771ea2bea85269
Souvik De <souvik.de@sato-global.com>
2024-07-22 16:36:24 +0900

    add leetcode 1 - Two Sum

---
+++ Daily-Leetcode/1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        mp<int, int> mp;
+        for(int i=0; i<n; i++){
+            int remaining = target - nums[i];
+            if(mp.find(remaining) != mp.end()){
+                return {mp[remaining], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
