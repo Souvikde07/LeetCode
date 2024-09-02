f6e89c18c86cfbc21056e950b912bf10d0dc590e
Souvik <souvik.de@sato-global.com>
2024-09-02 12:05:17 +0900

    add Leetcode 2022 - Convert 1D Array Into 2D Array

---
+++ Daily-Leetcode/2022.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (m * n != original.size()) {
+            return {};
+        }
+        
+        vector<vector<int>> res;
+        for (int i = 0; i < m; i++) {
+            res.push_back(vector<int>(original.begin() + i * n, original.begin() + (i + 1) * n));
+        }
+
+        return res;
+    }
+};
